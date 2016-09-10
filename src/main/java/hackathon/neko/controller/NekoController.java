a632d8e514bab58005aa98caaf77b425fd85e0ab
aya <aya.kawabe3@gmail.com>
2016-09-10 15:59:23 +0900

    logic for get data

--- src/main/java/hackathon/neko/controller/NekoController.java
+++ src/main/java/hackathon/neko/controller/NekoController.java
@@ -1,10 +1,15 @@
 package hackathon.neko.controller;
 
 import hackathon.neko.model.Data;
+import hackathon.neko.service.HtmlService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -13,6 +18,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class NekoController {
 
+    @Autowired
+    HtmlService htmlService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String get(){
         return "tmp";
@@ -20,8 +28,10 @@ public class NekoController {
 
     @ResponseBody
     @RequestMapping(value = "/data", method = RequestMethod.GET)
-    public Data neko(){
-        return new Data("title", "code", "result");
+    public List<Data> getData(){
+        List<String> result = htmlService.extractFromHTML();
+        List<Data> data= htmlService.convertToData(result);
+        return Arrays.asList(data.get(0));
     }
 
 }
---
+++ src/main/java/hackathon/neko/service/HtmlService.java
@@ -0,0 +1,80 @@
+package hackathon.neko.service;
+
+import hackathon.neko.model.Data;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@Service
+public class HtmlService {
+
+    public List<String> extractFromHTML() {
+        // WebサイトからHTMLを取得する
+        try {
+            URL url = new URL("https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array");
+            URLConnection uc = url.openConnection();
+
+            BufferedInputStream bis = new BufferedInputStream(uc.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(bis));
+
+            String line;
+            StringBuffer originalHTML = new StringBuffer();
+            while ((line = br.readLine()) != null) {
+                originalHTML.append(line);
+            }
+            return extractSection(originalHTML);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * HTMLから必要な部分だけ取り出す.
+     * @param originalHTML
+     * @return
+     */
+    private List<String> extractSection(StringBuffer originalHTML) {
+        String regex = "<p><strong>(.*?)</pre>";
+        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(originalHTML);
+        List<String> extractedSection = new ArrayList<String>();
+        while (matcher.find()) {
+            extractedSection.add(matcher.group().replaceAll("\t", "").replaceAll("&nbsp;", ""));
+        }
+        return extractedSection;
+    }
+
+    private String extractContent(String parseTargetStr, String checkPattern) {
+        Pattern pattern = Pattern.compile(checkPattern, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(parseTargetStr);
+        String matched = null;
+        if (matcher.find()) {
+            matched = matcher.group(1).replaceAll(";", ";<br>");
+        }
+        return matched;
+    }
+
+    public List<Data> convertToData(List<String> rawData) {
+        return rawData.stream()
+        .map(d -> {
+            String title = extractContent(d, "<p><strong>(.*?)</strong></p>");
+            String code = extractContent(d, "<pre class=\"brush: js\">(.*?)//");
+            String result = extractContent(d, "// (.*?)</pre>");
+            return StringUtils.isEmpty(title)
+                    || StringUtils.isEmpty(code)
+                    || StringUtils.isEmpty(result) ?
+                    null: new Data(title, code, result);
+        }).collect(Collectors.toList());
+    }
+}
